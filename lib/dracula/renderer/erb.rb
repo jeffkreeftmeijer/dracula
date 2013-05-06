f573d09274a2945e11d35df3b3e46d8df515f2dc
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2013-05-06 23:48:27 +0200

    Dracula::Renderer::ERB takes variables

--- lib/dracula/renderer/erb.rb
+++ lib/dracula/renderer/erb.rb
@@ -3,12 +3,14 @@ require 'erb'
 module Dracula
   module Renderer
     class ERB
-      def initialize(binding = binding)
-        @binding = binding
+      def initialize(data = {})
+        data.each do |key, value|
+          instance_variable_set("@#{key}", value)
+        end
       end
 
       def render(content)
-        ::ERB.new(content).result(@binding)
+        ::ERB.new(content).result(binding)
       end
     end
   end
--- test/renderer/erb_test.rb
+++ test/renderer/erb_test.rb
@@ -5,4 +5,8 @@ describe Dracula::Renderer::ERB do
   it "renders ERB" do
     Dracula::Renderer::ERB.new.render("<%= %w(foo bar baz).join(',') %>").should == "foo,bar,baz"
   end
+
+  it "uses passed variables" do
+    Dracula::Renderer::ERB.new(:foo => 'bar').render('<%= @foo %>').should == 'bar'
+  end
 end
