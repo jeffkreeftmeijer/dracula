d5bb848cef45fdec8bba6dbed4ec521528b5de42
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2016-12-28 14:32:16 +0100

    Indexer.index/1 handles file resource tuples in subdirectories

--- lib/dracula/indexer.ex
+++ lib/dracula/indexer.ex
@@ -5,8 +5,16 @@ defmodule Dracula.Indexer do
   ## Examples
     iex> Dracula.Indexer.index([{[], "index.html"}])
     {:ok, [%{"directory" => [], "path" => "index.html"}]}
+
+    iex> Dracula.Indexer.index([{["about"], "about/index.html"}])
+    {:ok, [%{"directory" => ["about"], "path" => "about/index.html"}]}
   """
-  def index([{directory, path}]) do
-    {:ok, [%{"directory" => directory, "path" => path}]}
+  def index(resources) do
+    index = resources
+    |> Enum.map(fn({directory, path}) ->
+      %{"directory" => directory, "path" => path}
+    end)
+
+    {:ok, index}
   end
 end
