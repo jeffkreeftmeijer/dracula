76555320e678cac122eac5a3de4dc1c4b420e9f5
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-01-08 13:51:37 +0100

    Renderer.layout renders layouts

--- lib/dracula/renderer.ex
+++ lib/dracula/renderer.ex
@@ -19,6 +19,12 @@ defmodule Dracula.Renderer do
     iex> Dracula.Renderer.render(%{"input_path" => "index.liquid", "contents" => "{{ name }}", "name" => "index.liquid"})
     "index.liquid"
 
+  Given a resource with a layout, the layout is rendered with the passed
+  contents as it's inner parameter.
+
+    iex> Dracula.Renderer.layout("index.md", %{"layouts" => ["layout [{{ inner }}]"]})
+    "layout [index.md]"
+
   """
   def render(%{"contents" => contents, "input_path" => path} = resource) do
     case Path.extname(path) do
@@ -30,5 +36,13 @@ defmodule Dracula.Renderer do
       ".md" -> Earmark.to_html(contents)
       _ -> contents
     end
+  end
+
+  def layout(contents, %{"layouts" => [layout]}) do
+    {:ok, rendered, _} = layout
+    |> Liquid.Template.parse
+    |> Liquid.Template.render(%{"inner" => contents})
+
+    rendered
   end
 end
