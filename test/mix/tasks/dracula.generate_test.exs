6766cbcd9aae28217780a55b73a72f58cccc34df
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-09-07 00:08:23 +0200

    Remove the _output directory before testing

--- test/mix/tasks/dracula.generate_test.exs
+++ test/mix/tasks/dracula.generate_test.exs
@@ -2,6 +2,11 @@ defmodule Mix.Tasks.Dracula.Generate.Test do
   use ExUnit.Case
   alias Mix.Tasks.Dracula.Generate
 
+  setup do
+    File.rm_rf!("_output")
+    :ok
+  end
+
   test "generates a static site" do
     Generate.run(["test/single_file"])
     assert File.read!("_output/index.html") == "<!-- index.html -->\n"
