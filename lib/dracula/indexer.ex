9af55911c21ab956166a44870806f9d473863431
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-09-07 22:36:27 +0200

    Indexer uses Renderer to render layouts

--- lib/dracula/indexer.ex
+++ lib/dracula/indexer.ex
@@ -2,6 +2,7 @@ defmodule Dracula.Indexer do
   @moduledoc """
   Indexes source directories.
   """
+  alias Dracula.Renderer
 
   @doc """
   Indexes a directory.
@@ -17,6 +18,30 @@ defmodule Dracula.Indexer do
   end
 
   defp index(input_path, output_path) do
-    {output_path, %{contents: File.read!(input_path)}}
+    index = %{input_path: input_path}
+    |> fetch_layout
+    |> fetch_contents
+
+    {output_path, index}
+  end
+
+  defp fetch_contents(%{input_path: input_path, layouts: layouts} = index) do
+    contents = input_path
+    |> File.read!
+    |> Renderer.render(input_path, layouts)
+
+    Map.put(index, :contents, contents)
+  end
+
+  defp fetch_layout(%{input_path: input_path} = index) do
+    layouts = case input_path
+    |> Path.dirname
+    |> Path.join("_layout.eex")
+    |> File.read do
+      {:ok, layout} -> [layout]
+      _ -> []
+    end
+
+    Map.put(index, :layouts, layouts)
   end
 end
--- test/dracula/indexer_test.exs
+++ test/dracula/indexer_test.exs
@@ -4,7 +4,21 @@ defmodule Dracula.IndexerTest do
 
   test "indexes a directory" do
     assert Indexer.index("test/single_file") == %{
-      "index.html" => %{contents: "<!-- index.html -->\n"}
+      "index.html" => %{
+        contents: "<!-- index.html -->\n",
+        input_path: "test/single_file/index.html",
+        layouts: []
+      }
+    }
+  end
+
+  test "indexes a directory with a layout file" do
+    assert Indexer.index("test/file_with_layout") == %{
+      "index.html" => %{
+        contents: "<!-- layout -->\n<!-- index.html -->\n\n",
+        input_path: "test/file_with_layout/index.html",
+        layouts: ["<!-- layout -->\n<%= @contents %>\n"]
+      }
     }
   end
 end
