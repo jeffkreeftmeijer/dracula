cbe93b9426c974b2ad324cd25d6897bd6eb5c91a
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-07-23 19:27:00 +0200

    Add failing test for index.html file generation

---
+++ dracula-tests.el
@@ -0,0 +1,13 @@
+(require 'dracula)
+
+(ert-deftest dracula-test-index-filenames ()
+  "Exports as index.html files to the _output directory."
+  (delete-directory "_output" t)
+  (org-publish-project "dracula-html" t)
+  (should (string-match-p "Dracula" (dracula-test-file-contents "_output/README/index.html"))))
+
+(defun dracula-test-file-contents (filename)
+  "Return the contents of FILENAME."
+  (with-temp-buffer
+    (insert-file-contents filename)
+    (buffer-string)))
