a7ad9d238aef4123695147a1f4be721496e8d339
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-07-23 20:15:07 +0200

    Publish to filename/index.html

--- dracula.el
+++ dracula.el
@@ -1,1 +1,13 @@
+(require 'ox-publish)
+(require 'ox-html-clean)
+
+(setq org-publish-project-alist
+      '(
+	("dracula-html"
+	 :base-directory "."
+	 :base-extension "org"
+	 :publishing-directory "_output"
+	 :recursive t
+	 :publishing-function org-html-clean-publish-to-html)))
+
 (provide 'dracula)
