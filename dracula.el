5efbce82a143f7b6c203cdbd0274afec273a3f55
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-07-24 15:39:25 +0200

    Add derived backend for Dracula

--- dracula.el
+++ dracula.el
@@ -2,6 +2,22 @@
 (require 'ox-html-clean)
 (require 'templatel)
 
+(defun org-dracula-html-template (contents info)
+  (org-html-template contents info))
+
+(org-export-define-derived-backend 'dracula-html 'html-clean
+  :translate-alist '((template . org-dracula-html-template)))
+
+(defun org-dracula-html-publish-to-html (plist filename pub-dir)
+  (advice-add 'org-export-output-file-name
+              :around #'html-clean-create-index-folder)
+  (org-publish-org-to 'dracula-html filename
+                      (concat "." (or (plist-get plist :html-extension)
+                                      org-html-extension "html"))
+                      plist pub-dir)
+  (advice-remove 'org-export-output-file-name
+                 #'html-clean-create-index-folder))
+
 (setq org-publish-project-alist
       '(
 	("dracula-html"
@@ -9,7 +25,7 @@
 	 :base-extension "org"
 	 :publishing-directory "_output"
 	 :recursive t
-	 :publishing-function org-html-clean-publish-to-html
+	 :publishing-function org-dracula-html-publish-to-html
 	 :html-doctype "html5"
 	 :html-head-include-default-style nil
 	 :html-head-include-scripts nil
